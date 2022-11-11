6ff7990cb80892966f4f144e52f05151659bf27e
kumar shanu <kshanu0602@gmail.com>
2022-11-11 10:03:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+ region = "us-east-2"
+}
+
+variable "image" {
+  type = string
+  default = "ami-0603cbe34fd08cb81"
+  description = "Enter your APP Image to be deployed"
+}
+
+variable "hw" {
+  type = string
+  default = "t2.nano"
+  description = "Enter your hardware model"
+}
+
+variable "name" {
+  type = string
+  default = "gagan-server1"
+  description = "Enter your server name"
+}
+
+resource "aws_instance" "myawsserver" {
+  ami = v
+ar.image
+  instance_type = var.hw
+
+  tags = {
+    Name = var.name
+    Env = "test"
+    Owner = "Gagandeep"
+  }
+}
+
+output "myawsserver-ip" {
+  value = aws_instance.myawsserver.public_ip
+}
